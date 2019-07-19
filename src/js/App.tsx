8540f7b3bb89c8a68b6048e77b11e947f65308c2
Mario Reder <mreder1289@gmail.com>
2019-07-19 15:49:52 +0200

    improved example app

--- src/js/App.tsx
+++ src/js/App.tsx
@@ -1,11 +1,13 @@
 import { h } from ".";
 
+import { Body } from "./Body";
+
 const App = (): JSX.Element => {
   console.log("APP");
   return (
     <div>
       <h1>Awesome Rust WASM App</h1>
-      <p>App mounted</p>
+      <Body />
     </div>
   );
 };
---
+++ src/js/Lorem.tsx
@@ -0,0 +1,18 @@
+import { h } from ".";
+
+const Lorem = (): JSX.Element => {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+      eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
+      voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
+      clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
+      amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+      nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed
+      diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
+      Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor
+      sit amet.
+    </div>
+  );
+};
+export default Lorem;
