475b276818c041b5bead191e6593a5377f74f5dd
Mario Reder <mreder1289@gmail.com>
2020-10-23 08:51:35 +0200

    use node 15 and extern refs

--- bench/smmdb/index.js
+++ bench/smmdb/index.js
@@ -1,28 +1,26 @@
 const React = require('react')
 const ReactIs = require('react-is')
 const ReactDOMServer = require('react-dom/server')
+const ReactWasmDOMServer = require('react-wasm-dom/server')
 const Benchmark = require('benchmark')
 
 const app = require('./app').getApp()
 
 const suite = new Benchmark.Suite()
 
-import('react-wasm-dom/server').then(async (module) => {
-  const renderToString = module.default.renderToString;
-  console.warn = () => {}
-  console.info = () => {}
-  console.error = () => {}
+console.warn = () => {}
+console.info = () => {}
+console.error = () => {}
 
-  app.getLocations().forEach(location => {
-    const jsx = app.jsx(location)
-    suite.add(`ReactWasmDOM#renderToString#loc"${location}"`, () => renderToString(React, ReactIs, jsx))
-    suite.add(`ReactDOM    #renderToString#loc"${location}"`, () => ReactDOMServer.renderToString(jsx))
-  })
-
-  suite
-    .on('cycle', event => {
-      console.log(String(event.target));
-    })
-    .run({ async: true })
+app.getLocations().forEach(location => {
+  const jsx = app.jsx(location)
+  suite.add(`ReactWasmDOM#renderToString#loc"${location}"`, () => ReactWasmDOMServer.renderToString(React, ReactIs, jsx))
+  suite.add(`ReactDOM    #renderToString#loc"${location}"`, () => ReactDOMServer.renderToString(jsx))
 })
+
+suite
+  .on('cycle', event => {
+    console.log(String(event.target));
+  })
+  .run({ async: true })
 
